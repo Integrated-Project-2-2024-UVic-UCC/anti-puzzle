a772940d76de9686340eef216a4e156c899c9bb0
CarlesNV11 <158203662+CarlesNV11@users.noreply.github.com>
2024-04-02 18:32:52 +0200

    Update bom.csv

---
+++ Programming/Codis/Codi prova finals de carera/Codi prova finals de carera.ino
@@ -0,0 +1,24 @@
+// Per connectar a l'aruidno COM al pin digital, NO a terra i NC a 5V
+
+#define PIN_FIN_CARRERA 2 // Pin digital al que se conecta el final de carrera
+
+
+void setup() {
+  Serial.begin(9600); // Iniciar comunicación serial
+  pinMode(PIN_FIN_CARRERA, INPUT); // Configurar el pin como entrada
+}
+
+void loop() {
+  int estado = digitalRead(PIN_FIN_CARRERA); // Leer el estado del final de carrera
+  
+  Serial.print("Estado del final de carrera: ");
+  if (estado == HIGH) {
+    Serial.println("Abierto");
+  } else {
+    Serial.println("Cerrado");
+  }
+  
+  delay(500); // Esperar 500 milisegundos antes de la siguiente lectura
+}
+
+
