d5d94f0277626da1ecc8b7e0d729d9106c0aa83d
QuimGr <joaquim.graells@uvic.cat>
2024-05-10 23:39:21 +0200

    Anti-Puzzle code V1.5
    CI -- OK, Home -- OK, Program -- Testing

--- Programming/Codis/Anti_Puzzle/Anti_Puzzle.ino
+++ Programming/Codis/Anti_Puzzle/Anti_Puzzle.ino
@@ -1,12 +1,20 @@
 #include <util/atomic.h>
 #include <EEPROM.h>
 
+// Pins motor X
 #define X_STEP_PIN 9
 #define X_DIR_PIN 3
-#define X_ENABLE_PIN 4 // Define el pin de habilitación para el motor en el eje X
+#define X_ENABLE_PIN 4
+
+// Pins motor Y
 #define Y_STEP_PIN 11
 #define Y_DIR_PIN 6
-#define Y_ENABLE_PIN 7 // Define el pin de habilitación para el motor en el eje Y
+#define Y_ENABLE_PIN 7
+
+#define DX_epin 10 // Nuevo detector de final de carrera en el eje X
+#define DY_spin 8  // Nuevo detector de final de carrera en el eje Y
+#define DX_dpin A4 // Nuevo detector de final de carrera en el eje X
+#define DY_ipin A5 // Nuevo detector de final de carrera en el eje Y
 
 // Direcciones en la memoria EEPROM donde se guardarán las coordenadas
 #define EEPROM_X_ADDRESS 0
@@ -31,7 +39,7 @@ void Guardar_Memoria(int x, int y) {
   int posX = EEPROM.read(EEPROM_X_ADDRESS);
   int posY = EEPROM.read(EEPROM_Y_ADDRESS);
   
-  if (posX != x || posY != y) {
+  if (posX != x && posY != y) {
     EEPROM.write(EEPROM_X_ADDRESS, x);
     EEPROM.write(EEPROM_Y_ADDRESS, y);
   } 
@@ -82,21 +90,50 @@ void configurarTimer2(unsigned int frecuenciaDeseada) {
   TCCR2B |= (1 << CS22);
   TIMSK2 |= (1 << OCIE2A); // Habilitamos la interrupción de comparación del Timer 2
 }
+
 // ················································
 // Habilita i deshabilita motors
 // ················································
 
-void enableMotorX() {digitalWrite(X_ENABLE_PIN, LOW);} 
+void enableMotorX() {
+  digitalWrite(X_ENABLE_PIN, LOW);
+} 
 
-void disableMotorX() {digitalWrite(X_ENABLE_PIN, HIGH);} 
+void disableMotorX() {
+  digitalWrite(X_ENABLE_PIN, HIGH);
+} 
 
-void enableMotorY() {digitalWrite(Y_ENABLE_PIN, LOW);} 
+void enableMotorY() {
+  digitalWrite(Y_ENABLE_PIN, LOW);
+} 
 
-void disableMotorY() {digitalWrite(Y_ENABLE_PIN, HIGH);} 
+void disableMotorY() {
+  digitalWrite(Y_ENABLE_PIN, HIGH);
+} 
 
 // ################################################
 // Geters
 // ################################################
+
+int getDX_d(){
+  int DX_d = digitalRead(DX_dpin); // Cambio el pin al detector nuevo
+  return DX_d;
+}
+
+int getDX_e(){
+  int DX_e = digitalRead(DX_epin); // Cambio el pin al detector nuevo
+  return DX_e;
+}
+
+int getDY_i(){
+  int DY_i = digitalRead(DY_ipin); // Cambio el pin al detector nuevo
+  return DY_i;
+}
+
+int getDY_s(){
+  int DY_s = digitalRead(DY_spin); // Cambio el pin al detector nuevo
+  return DY_s;
+}
 
 long getStepsX(void){
   long aux;
@@ -129,6 +166,7 @@ long getSpeedY(){
   };
   return speedY;
 }
+
 // ################################################
 // Seters 
 // ################################################
@@ -151,41 +189,19 @@ void setSpeedMotorX(float speed){
 void setSpeedMotorY(float speed){
   OCR2A = speed;
   TCNT2 = 0;
-}
+} 
 
-void Home(int X, int Y) {
-  int homeX = (8250)/2; // Mitad de la coordenada X que son 8250 micropasos --> 3200*2.57
-  int homeY = (4570)/2; // Mitad de la coordenada Y que son 4570 micropasos
-
-  int Ix = homeX - X;
-  int Iy = homeY - Y;
-
-  float Vx = Ix / 5.0;
-  float Vy = Iy / 5.0; 
-
-  // Paso 1: Mover el motor X a la posición de inicio en X
-  setDirectionMotorX(X < homeX); 
-  setDirectionMotorY(Y < homeY); 
-
-  setSpeedMotorX(Vx);
-  setSpeedMotorY(Vy);
-
-  enableMotorX();
-  enableMotorY();
-
-  while (!(getStepsX() == Ix)){} // && getStepsY() == Iy)) {}
-
-  disableMotorX();
-  disableMotorY();
-}
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Programa  ---  CI. ---  HOME. ----  PosicioIman.  
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 void CI() {
   // Paso 1: Mover los motores a la mitad de las coordenadas
-  int mitadX = (8250)/2; // Mitad de la coordenada X que son 8250 micropasos --> 3200*2.57
-  int mitadY = (4570)/2; // Mitad de la coordenada Y que son 4570 micropasos
+  int mitadX = (15300)/2; // Mitad de la coordenada Y que son 15300 micropasos
+  int mitadY = (8200)/2; // Mitad de la coordenada X que son 8200 micropasos --> 3200
 
-  float Vx = mitadX / 50.0;
-  float Vy = mitadY / 50.0;
+  int Vx = mitadY / 75;
+  int Vy = mitadX / 75;
 
   setDirectionMotorX(true);
   setDirectionMotorY(true);
@@ -196,23 +212,74 @@ void CI() {
   enableMotorX();
   enableMotorY();
 
-  while ((getStepsX() < mitadX)){}// && (getStepsY() < mitadY)) {}
+  while ((getStepsY() < mitadY) && (getStepsX() < mitadX)) {}
 
   disableMotorX();
   disableMotorY();
 
   // Paso 2: Mover el motor X hacia la derecha para completar los 210mm
   // Calcular los pasos necesarios para recorrer los 210mm en X
+  int pasX = getStepsX() + mitadX;
   enableMotorX();
-  while ((getStepsX() < mitadX)){
-    int difdist = pasosX - getStepsX();
-    float velocidadX = map(difdist, 0, mitadX, 225, 16);
+  while ((getStepsX() < pasX)){
+    delay(100);
+    int difdist = pasX - getStepsX();
+    float velocidadX = map(difdist, 0, pasX, 225, 16);
     setSpeedMotorX(velocidadX);
   }
   disableMotorX();
 
   // Paso 3: Guardar las coordenadas en la memoria EEPROM
   Guardar_Memoria(getStepsX(), getStepsY());
+}
+
+void Home() {
+  int homeX = (15418)/2; // Mitad de la coordenada Y que son 15300 micropasos
+  int homeY = (8200)/2; // Mitad de la coordenada X que son 8200 micropasos --> 3200 4100
+
+  int Vx = homeY / 75;
+  int Vy = homeX / 75;
+
+  // Paso 1: Mover el motor X a la posición de inicio en X
+  setDirectionMotorX(false); 
+  setDirectionMotorY(false); 
+
+  setSpeedMotorX(100);
+  setSpeedMotorY(100);
+
+  enableMotorX();
+
+  // Mover motor X
+  while (!getDX_d() || getDX_e()) {} 
+  disableMotorX();
+
+  enableMotorY();
+
+  // Mover motor Y
+  while (!getDY_i() || getDY_s()) {}
+  disableMotorY();
+
+  setSpeedMotorX(Vx);
+  setSpeedMotorY(Vy);
+
+  if (getDX_d() == HIGH){
+    setDirectionMotorX(false); 
+  }else if (getDX_e() == HIGH){
+    setDirectionMotorX(true);
+  }
+  
+  if (getDY_i() == HIGH){
+    setDirectionMotorY(false);
+  }else if (getDY_s() == HIGH){
+    setDirectionMotorY(true);
+  }
+
+  enableMotorX();
+  enableMotorY();
+  while (getStepsY() < homeX){}//&& (getStepsY() <= homeY)) {}
+
+  disableMotorX();
+  disableMotorY();
 }
 
 int PosicioIman(){
@@ -228,22 +295,19 @@ int PosicioIman(){
   bool dirX = EX > 0; // Si errX es positivo, el imán se mueve en la dirección positiva de X
   bool dirY = EY > 0; // Si errY es positivo, el imán se mueve en la dirección positiva de Y
 
-  float moveX = map(EX, 505, 1024, 0, 8250);
-  float moveY = map(EY, 505, 1024, 0, 4570);
+  float moveX = map(EX, 505, 1024, 0, 8250); // Es posible que els valors dels pasos s'haigui de cambiar perque no faigui un salt tant gran 
+  float moveY = map(EY, 505, 1024, 0, 4570); // Es posible que els valors dels pasos s'haigui de cambiar perque no faigui un salt tant gran 
 
-  if (moveX == 0 && moveY == 0) {
-    // Guardar la posición actual de los motores en la memoria EEPROM
-    Guardar_Memoria(getStepsX(), getStepsY());
-  } else {
-    // Mover los motores a las nuevas coordenadas
-    Movemotors(moveX, moveY, dirX, dirY);
-  }
+  if (moveX == 0 && moveY == 0) {Guardar_Memoria(getStepsX(), getStepsY());} 
+  
+  Movemotors(moveX, moveY, dirX, dirY);
 }
 
 int CalculerrorX(int hall1,int hall2,int hall3,int hall4){
   int errX = (hall1 + hall2) - (hall3 + hall4);
   return errX;
 }
+
 int CalculerrorY(int hall1,int hall2,int hall3,int hall4){
   int errY = (hall1 + hall4) - (hall2 + hall3);
   return errY;
@@ -259,8 +323,8 @@ void Movemotors(float posX, float posY, bool dirX, bool dirY) {
 
 void Movepos(float stepsX, bool dirX, float stepsY, bool dirY) {
   
-  float Vx = stepsX / 5.0;
-  float Vy = stepsY / 5.0; 
+  int Vx = stepsY / 75;
+  int Vy = stepsX / 75;
 
   setDirectionMotorX(dirX);
   setDirectionMotorY(dirY);
@@ -268,32 +332,37 @@ void Movepos(float stepsX, bool dirX, float stepsY, bool dirY) {
   enableMotorX();
   enableMotorY();
 
-  while (!(getStepsX() >= stepsX && getStepsY() >= stepsY)) {
+  while (getStepsX() < stepsX ){//&& getStepsY() >= stepsY)) {
     // Configurar la velocidad del motor X en función de la distancia restante
+    delay(100);
     float velX = map(stepsX - getStepsX(), 0, stepsX, 225, Vx);
     float velY = map(stepsY - getStepsY(), 0, stepsY, 225, Vx);
     setSpeedMotorX(velX);
     setSpeedMotorY(velY);
   }
 }
+
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 // Interrupcions
 // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
 // Función para manejar la interrupción del Timer 1
-ISR(TIMER1_COMPA_vect) {if(dirX) {totalStepsX++;} else {totalStepsX--;}}
+ISR(TIMER1_COMPA_vect) {
+  if(dirX) {totalStepsX++;} else {totalStepsX--;}
+}
 
 // Función para manejar la interrupción del Timer 2
-ISR(TIMER2_COMPA_vect) {if(dirY) {totalStepsY++;} else {totalStepsY--;}}
+ISR(TIMER2_COMPA_vect) {
+  if(dirY) {totalStepsY++;} else {totalStepsY--;}
+}
 
 // 
 // Codi Principal
 // 
+
 void setup() {
   Serial.begin(9600);
 
-  int posX = EEPROM.read(EEPROM_X_ADDRESS);
-  int posY = EEPROM.read(EEPROM_Y_ADDRESS);
-  
   pinMode(X_STEP_PIN, OUTPUT);
   pinMode(X_DIR_PIN, OUTPUT);
   pinMode(X_ENABLE_PIN, OUTPUT);
@@ -301,22 +370,30 @@ void setup() {
   pinMode(Y_DIR_PIN, OUTPUT);
   pinMode(Y_ENABLE_PIN, OUTPUT); 
 
+  pinMode(DX_epin, OUTPUT);
+  pinMode(DY_spin, OUTPUT);
+  pinMode(DX_dpin, OUTPUT);
+  pinMode(DY_ipin, OUTPUT);
+
   pinMode(hall1Pin, INPUT);
   pinMode(hall2Pin, INPUT);
   pinMode(hall3Pin, INPUT);
   pinMode(hall4Pin, INPUT);
+
+  pinMode(DX, INPUT_PULLUP); // Configura el pin DX como entrada con resistencia de pull-up
+  pinMode(DY, INPUT_PULLUP);
   
-  configurarTimer1(1); 
-  configurarTimer2(255);
+  configurarTimer1(255); 
+  configurarTimer2(1);
+  disableMotorX();
+  disableMotorY();
 
   totalStepsX = 0;
   totalStepsY = 0;
-  
-  enableMotorX();
-  enableMotorY();
+  // CI();
+  Home();
 }
+
 void loop() {
-  CI();
-  // Home();
   // PosicioIman();
 }
